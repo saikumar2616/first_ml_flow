460b554ad5664f66ac6d9fac16acefc6bf6e2fd2
saikumar <sirigirisaikumar0@gmail.com>
2023-08-03 16:23:26 +0530

    remote tracking

---
+++ README.md
@@ -0,0 +1,7 @@
+##ML FLow experiments
+
+
+MLFLOW_TRACKING_URI=https://dagshub.com/saikumar2616/first_ml_flow.mlflow \
+MLFLOW_TRACKING_USERNAME=saikumar2616 \
+MLFLOW_TRACKING_PASSWORD=a942784accdc1d890f0ec526a3bcbea789234e30 \
+python script.py
--- app.py
+++ app.py
@@ -78,6 +78,9 @@ if __name__ == "__main__":
         predictions = lr.predict(train_x)
         signature = infer_signature(train_x, predictions)
 
+        #rEMOTE sERVER
+        remote_server_url="https://dagshub.com/saikumar2616/first_ml_flow.mlflow"
+        mlflow.set_tracking_uri(remote_server_url)
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
